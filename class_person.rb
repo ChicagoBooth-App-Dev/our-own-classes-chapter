8f4aa9ea61b369f467df6a869dd0e5d80d651621
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-08 17:40:14 +0000

    passed all tests

--- class_person.rb
+++ class_person.rb
@@ -7,5 +7,21 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
-  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + ' ' + self.last_name
+  end
+
+  require('date')
+
+  def age
+    dob = Date.parse(self.birthdate)
+    today = Date.today
+    return ((today - dob)/365).to_i
+  end
+
 end
+
